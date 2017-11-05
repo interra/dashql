7f322cbafbbca6b52b47f6a188a18431bd6289ef
pj <starsinmypockets@gmail.com>
2017-11-04 20:58:38 -0400

    Add switch to resolvers Stubs for multiple backends Fetches data based on dataHandlers schema for carto

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,109 +1,69 @@
-const { makeExecutableSchema } = require('graphql-tools');
-const resolvers = require('./resolvers');
+const JSON = require('graphql-type-json')
+const { makeExecutableSchema } = require('graphql-tools')
+const resolvers = require('./resolvers')
 
 const typeDefs = `
 ##
 # DATA RESOURCES
 ##
-enum dataResourceType {
-	cartodb
-	csv
-	dkan
-	ckan
+
+enum DataResourceType {
+  cartodb
+  ckan
+  dkan
+  postgres
+}
+
+enum ResponseFieldType {
+  number
+  string
+  boolean
+  date
+  geometry
+}
+
+type ResponseField {
+  fieldName: String
+  fieldType: ResponseFieldType
+}
+
+type Response {
+  rows: [String] # array of json strings containing response data
+  total_rows: Int
+  time: Float
+  fields: [ResponseField]
 }
 
 type DataResource {
-	dataResourceType: dataResourceType!
-	resourceHandle: String!
-	url: String
+  type: DataResourceType!
+  resourceHandle: String!
+  response: Response
 }
 
-##
-# COMPONENTS
-##
-type ComponentDataField {
-	fieldName: String #the name to apply to the component's data for this field value
-	dataResourceHandle: String! # References a valid dataResource
-	# @@TODO could dataResourceField be replaced by a GraphQL query?
-	dataResourceField: String # Reference to extant field on dataResource
+enum Op {
+  LT
+  GT
+  IN
 }
 
-enum filterType {
-	Autocomplete
-	Checkbox
+input Filter {
+  op: Op # valid sql operator
+  field: String # field on dataResource
+  type: String # this is enum in resolver
+  vals: String # serialized JSON
 }
 
-type FilterOption {
-	label: String
-	value: String
+input Resource {
+  resourceHandle: String! # used to generate memory store
+  type: String! # this is enum in resolvers
+  url: String!
+  q: String
+  json: Boolean
 }
 
-# from https://www.w3schools.com/sql/sql_operators.asp
-enum FilterOperation {
-	## Logical Operators
-	ALL	#TRUE if all of the subquery values meet the condition
-	AND	#TRUE if all the conditions separated by AND is TRUE
-	ANY	#TRUE if any of the subquery values meet the condition
-	BETWEEN	#TRUE if the operand is within the range of comparisons
-	EXISTS	#TRUE if the subquery returns one or more records
-	IN	#TRUE if the operand is equal to one of a list of expressions
-  NOTIN
-	LIKE	#TRUE if the operand matches a pattern
-	NOT	#Displays a record if the condition(s) is NOT TRUE
-	OR	#TRUE if any of the conditions separated by OR is TRUE
-	SOME  #	TRUE if any of the subquery values meet the condition
-
-	## Comparison Operators
-	EQ	#Equal to
-	GT	#Greater than
-	LT  #Less than
-	GTEQ #Greater than or equal to
-	LTEQ #Less than or equal to
-	NOTEQ #Not equal to
+type Query {
+  getDataResources(resources: [Resource], filter: [Filter]): [DataResource]
 }
+`
 
-type Filter {
-	type: filterType
-  class: String
-	filterKey: String! # this needs to be unique amongst fields
-	label: String # html label
-	willFilter: String # a valid dataResourceHandle
-	multi: Boolean # allow multiple filter values
-	# @@UMMM could this be a GraphQL query?
-	dataResourceField: String # @@UMMM this references a valid
-	operation: FilterOperation
-	options: [FilterOption]
-}
-
-enum visualizationType {
-	Chart
-	Metric
-}
-
-type Component {
-	type: visualizationType
-	class: String
-}
-
-##
-# REGIONS
-##
-
-type Region {
-	id: ID
-	children: [Component]! 
-}
-
-type Dashboard {
-	title: String
-	dataResources: [DataResource]
-  filters: [Filter]
-	regions: [Region]
-}
-
-  type Query {
-      getDash: Dashboard
-  }
-`;
-
-    module.exports = makeExecutableSchema({ typeDefs, resolvers });
+module.exports = makeExecutableSchema({ typeDefs, resolvers })
--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,12 +1,74 @@
 const _ = require('lodash');
 // Sample data
 const { dash } = require('./../data/data');
+const rp = require('request-promise')
 
 const resolvers = {
     Query: {
-        // Fetch all tasks
-        getDash: () => dash,
-    },
-};
+      // fetch data
+      // and parse as cartodb api response
+      getDataResources: (_, {resources}) => {
+        const all = resources.map(resource => {
+        
+        switch (resource.type) {
+          case 'cartodb':
+            return _fetchCartoResource(resource)
+
+          default:
+            return {
+              type: 'unknown', // this will throw graphQL type exception
+              resourceHandle: resource.resourceHandle
+            }
+        }
+      })
+
+      return Promise.all(all)
+    }
+  }
+}
+
+const _fetchCartoResource = module.exports._fetchCartoResource = (resource) => {
+    return new Promise((resolve, reject) => {
+      rp({
+        uri: resource.url + resource.q,
+        json: resource.json
+      }).then(json => {
+        const response = _parseCartoResponse(json)
+        const dataResource =
+        {
+          response: response,
+          type: 'cartodb',
+          resourceHandle: resource.resourceHandle
+        }
+        resolve(dataResource)
+      }).catch(err => {
+        reject(err)
+      })
+    })
+}
+
+const _parseCartoResponse = module.exports._parseCartoResponse = (_r) => {
+  const response = JSON.parse(_r)
+  const rows = response.rows.map(JSON.stringify)
+  const total_rows = parseInt(response.total_rows)
+  const time = parseFloat(response.time)
+  const _fields = Object.keys(response.fields)
+  const fields = _fields.map(field => {
+    return {
+      fieldName: field,
+      fieldType: response.fields[field].type
+    }
+
+  })
+  console.log(rows, total_rows, time, fields)
+
+  return {
+    type: 'cartodb',
+    rows: rows,
+    total_rows: total_rows,
+    time: time,
+    fields: fields
+  }
+}
 
 module.exports = resolvers;
