a03f2bce5e6798dad92cbd00f4b95b1ca862c1c1
pj <starsinmypockets@gmail.com>
2017-11-14 15:45:52 -0500

    Handle cors for dev

--- src/server.js
+++ src/server.js
@@ -2,10 +2,26 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 const schema = require('./schema/index')
-
+const app = express()
 const APP_PORT = 3333
 
-const app = express()
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    console.log('REQ METHOD', req.method)
+
+    //intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+      //respond with 200
+      res.send(200);
+    }
+    else {
+    //move on
+      next();
+    }
+});
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
 app.use('/graphiql', graphiqlExpress({ endpointURL: 'graphql' }))
